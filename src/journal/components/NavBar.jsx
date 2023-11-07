612c5fe984460cfcf1b0e5124900bc1205ed0c6d
king-beard <chucho_herrera10@icloud.com>
2023-11-07 08:36:05 -0600

    Add NavBar

---
+++ src/journal/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material"
+import { LogoutOutlined, MenuOutlined } from "@mui/icons-material"
+
+export const NavBar = ({ drawerWith }) => {
+  return (
+    <AppBar
+      position="fixed"
+      sx={{ width : { sm : `calc(100% - ${ drawerWith })` } }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            edge="start"
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuOutlined />
+          </IconButton>
+          <Grid container direction="row" justifyContent="space-between" alignItems="center">
+            <Typography variant="h6" noWrap component="div"> JournalApp </Typography>
+            <IconButton color="error">
+              <LogoutOutlined />
+            </IconButton>
+          </Grid>
+        </Toolbar>
+    </AppBar>
+  )
+}
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,8 +1,12 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components/NavBar";
+
+const drawerWith = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display : "flex" }}>
+      <NavBar drawerWith={ drawerWith } />
       <Box
         component="main"
         sx={{ flexGrow : 1, p: 3 }}
