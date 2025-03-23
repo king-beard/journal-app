c8e9d2a5c25d0a28fd9deafca8353bc9f566a434
king-beard <chucho_herrera10@icloud.com>
2025-03-22 23:43:45 -0600

    JournalLayout and JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
-import { Fragment } from "react"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Fragment>
-      <Typography variant="h1">JournalPage</Typography>
-    </Fragment>
+    <JournalLayout>
+      <Typography>
+        Ut excepteur esse Lorem aliquip occaecat fugiat occaecat in amet proident. Tempor sint exercitation ea labore. Aliqua labore duis voluptate reprehenderit ut ad irure consectetur cupidatat eu.
+      </Typography>
+    </JournalLayout>
   )
 }
